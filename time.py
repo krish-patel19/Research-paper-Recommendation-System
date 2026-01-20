d40c614149591252f694ec98cd6cec715fe77fc7
krish-patel19 <krishpatel192005@gmail.com>
2026-01-20 10:49:36 +0530

    Add files via upload

---
+++ time.py
@@ -0,0 +1,6 @@
+# This file is for Python code, not for pip install commands
+# To install packages, run these commands in the terminal:
+# pip3 install tensorflow==2.15.0
+# pip3 install torch==2.0.1
+# pip3 install sentence_transformers==2.2.2
+# pip3 install streamlit
